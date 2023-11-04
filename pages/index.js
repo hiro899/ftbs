edeca8f617728f65419f1fd9068151e68d2008d7
hiro899 <125556630+hiro899@users.noreply.github.com>
2023-11-04 13:37:01 +0900

    add button and change to japanese

--- pages/index.js
+++ pages/index.js
@@ -27,35 +27,42 @@ function HomePage () {
           <a
             className="w-64 text-center block px-10 py-3 text-white rounded transition-colors duration-200 bg-[#775541] hover:bg-[#59382D]"
           >
-            Order Sheets
+          卸リスト
           </a>
         </Link>
         <Link href="https://docs.google.com/forms/d/e/1FAIpQLScI9wUcWgwo-cH5UcmcSUTGeCerEqm30yIykRDWNpXOSJQuxA/viewform">
           <a
             className="w-64 text-center block px-10 py-3 text-white rounded transition-colors duration-200 bg-[#775541] hover:bg-[#59382D]"
           >
-            Order Form
+            オーダーフォーム
           </a>
         </Link>
         <Link href="https://docs.google.com/forms/d/1u0deLkB3tYzhXu5TM-Xy-8PitHQ1SyPTXTBRyk0lE6Q/viewform?edit_requested=true">
           <a
             className="w-64 text-center block px-10 py-3 text-white rounded transition-colors duration-200 bg-[#775541] hover:bg-[#59382D]"
           >
-            Terms Confirmation
+            仕入条件確認
           </a>
         </Link>
         <Link href="https://docs.google.com/forms/d/1_Yw-0_qZ5X9UDXlW8AtdnxO0qXDzZZeosW6sAqWYas4/viewform?edit_requested=true">
           <a
             className="w-64 text-center block px-10 py-3 text-white rounded transition-colors duration-200 bg-[#775541] hover:bg-[#59382D]"
           >
-            Contact Us
+            お問い合わせ
+          </a>
+        </Link>
+        <Link href="https://docs.google.com/presentation/d/1tFmLvUjpLyC--YIPr1M6jqhM6AuR3S_IQuW9-CDzziE/edit#slide=id.g296526db04c_0_195">
+          <a
+            className="w-64 text-center block px-10 py-3 text-white rounded transition-colors duration-200 bg-[#775541] hover:bg-[#59382D]"
+          >
+            サイトの使い方
           </a>
         </Link>
         <button 
           onClick={handleLogout} 
           className="text-[#775541] border border-[#775541] hover:text-white hover:bg-[#59382D] transition-colors duration-200 px-3 py-2 rounded"
         >
-          Logout
+          ログアウト
         </button>
       </div>
     </div>
